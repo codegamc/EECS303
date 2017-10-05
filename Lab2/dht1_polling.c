d93d2e379b141b2911996952208a660ca7b849fa
Nate Lundie <ngl8@case.edu>
2017-10-05 18:09:50 -0400

    change

--- Lab2/dht1_polling.c
+++ Lab2/dht1_polling.c
@@ -70,13 +70,13 @@ void read_dht11_dat()
  		count = count + 1;
  		f = dht11_dat[2] * 9. / 5. + 32;
  		
- 		printf("Time: %s humidity = %d %% temp = %d C (%f F)\n", ctime(time(NULL)), dht11_dat[0], dht11_dat[2], f);
+ 		printf("Time: %s humidity = %d %% temp = %d C (%f F)\n", ctime(&time(NULL)), dht11_dat[0], dht11_dat[2], f);
  		//FILE *fp;
  		
  		//if(fp == NULL)
  		//{
  			
- 			fprintf(fp, "Time: %s humidity = %d %% temp = %d C (%f F)\n", ctime(time(NULL)), dht11_dat[0], dht11_dat[2], f);
+ 			fprintf(fp, "Time: %s humidity = %d %% temp = %d C (%f F)\n", ctime(&time(NULL)), dht11_dat[0], dht11_dat[2], f);
  			fclose(fp);
  		//}
  	}
