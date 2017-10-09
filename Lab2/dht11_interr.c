3420ed3b49d9e5cff22976b6516cf483fd762b60
Nate Lundie <ngl8@case.edu>
2017-10-09 16:38:04 -0400

    check

--- Lab2/dht11_interr.c
+++ Lab2/dht11_interr.c
@@ -203,11 +203,11 @@ void sensorReadISR()
 			if(prevBitHighTime > 40)
 			{
 				bitsRcvd[currentReadingBitIdx - 1] = 1;
-				printf("bit #%d is :%u\n", currentReadingBitIdx - 1, 1);
+				//printf("bit #%d is :%u\n", currentReadingBitIdx - 1, 1);
 			}	
 			else{
 				bitsRcvd[currentReadingBitIdx - 1] = 0;
-				printf("bit #%d is :%u\n", currentReadingBitIdx - 1, 0);
+				//printf("bit #%d is :%u\n", currentReadingBitIdx - 1, 0);
 			}
 				
 			++currentReadingBitIdx;
@@ -328,19 +328,19 @@ void analyzeAndPrintResults(int * bitsRcvd, const char * errorString, const char
 
 
 
-	//if(checksum_generated == checksum_read)
-	//{
+	if(checksum_generated == checksum_read)
+	{
 		// Print the results
 		printf("Temp: %d.%d\n", temp_int, temp_dec);
 		printf("Humidity: %d.%d\n", humid_int, humid_dec);
 		
 		// Write the results to a file
 		writeResultsToFile(temp_int, temp_dec, humid_int, humid_dec, checksum_read, checksum_generated, sensorInteractionMode, timeAsString, errorString);
-	//}
-	//else
-	//{
-	//	printf("Checksum failed");
-	//}
+	}
+	else
+	{
+		printf("Checksum failed");
+	}
 }
 
 /*
