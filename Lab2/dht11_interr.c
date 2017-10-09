6f07fbc6055b6fd997d305e5fefa5d7f3b75b25f
Nate Lundie <ngl8@case.edu>
2017-10-09 16:29:23 -0400

    &

--- Lab2/dht11_interr.c
+++ Lab2/dht11_interr.c
@@ -322,7 +322,7 @@ void analyzeAndPrintResults(int * bitsRcvd, const char * errorString, const char
 
 
 	// Check checksum
-	int checksum_generated = generateChecksum({temp_int, temp_dec, humid_int, humid_dec});
+	int checksum_generated = generateChecksum(&{temp_int, temp_dec, humid_int, humid_dec});
 	
 
 
