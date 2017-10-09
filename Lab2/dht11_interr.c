5c2289277e50a544213a1d4533d6fc81332d7545
Nate Lundie <ngl8@case.edu>
2017-10-09 15:48:15 -0400

    test

--- Lab2/dht11_interr.c
+++ Lab2/dht11_interr.c
@@ -313,19 +313,19 @@ void analyzeAndPrintResults(int * bitsRcvd, const char * errorString, const char
 	int humid_dec = bitsRcvd[3];
 	int checksum_read = bitsRcvd[4];
 
-	if(checksum_generated == checksum_read)
-	{
+	//if(checksum_generated == checksum_read)
+	//{
 		// Print the results
 		printf("Temp: %d.%d\n", temp_int, temp_dec);
 		printf("Humidity: %d.%d\n", humid_int, humid_dec);
 		
 		// Write the results to a file
 		writeResultsToFile(temp_int, temp_dec, humid_int, humid_dec, checksum_read, checksum_generated, sensorInteractionMode, timeAsString, errorString);
-	}
-	else
-	{
-		printf("Checksum failed");
-	}
+	//}
+	//else
+	//{
+	//	printf("Checksum failed");
+	//}
 }
 
 /*
