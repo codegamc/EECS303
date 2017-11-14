8da4cfcfb470a4aa6f9c7cc10bca8503f742055c
Michael <michael.c.codega@gmail.com>
2017-11-14 16:38:26 -0500

    done

--- Lab4_new/pwm2.s
+++ Lab4_new/pwm2.s
@@ -1,89 +1,53 @@
 
-	.global _start
-	.extern	ledPin
+	.global main
+
 	.extern wiringPiSetup
 	.extern pinMode
 	.extern softPwmCreate
 	.extern softPwmWrite
-	.section	.rodata
-	.align	2
-	.type	ledPin, %object
-	.size	ledPin, 4
-ledPin:
-	.word	7
-	.global	brightness
-	.bss
-	.align	2
-	.type	brightness, %object
-	.size	brightness, 4
-brightness:
-	.space	4
-	.text
-	.align	2
-	.global	main
-	.type	main, %function
-_start:
-	@ args = 0, pretend = 0, frame = 0
-	@ frame_needed = 1, uses_anonymous_args = 0
-	stmfd	sp!, {fp, lr}
-	add	fp, sp, #4
+	.extern delay
+	.balign 4
+	
+
+main:
+	
+	push {ip, lr}
 	bl	wiringPiSetup
-	mov	r3, #7
-	mov	r0, r3
+	mov	r4, #7
+	mov	r0, r4
 	mov	r1, #1
 	bl	pinMode
-	mov	r3, #7
-	mov	r0, r3
+	mov	r4, #7
+	mov	r0, r4
 	mov	r1, #0
 	mov	r2, #100
 	bl	softPwmCreate
-	mov r3, #0
+	mov r4, #0
 	b	.L2
 .L3:
 	mov	r0, #7
-	@ldr	r3, .L7
-	@ldr	r3, [r3]
-	@mov	r0, r2
-	mov	r1, r3
+	mov	r1, r4
 	bl	softPwmWrite
-	@ldr	r3, .L7
-	@ldr	r3, [r3]
-	add	r3, r3, #1
-	@ldr	r2, .L7
-	@str	r3, [r2]
+	add	r4, r4, #1
 	mov	r0, #40
 	bl	delay
 .L2:
-	@ldr	r3, .L7
-	@ldr	r3, [r3]
-	cmp	r3, #99
+	cmp	r4, #99
 	ble	.L3
 	b	.L4
 .L5:
 	mov	r0, #7
-	@ldr	r3, .L7
-	@ldr	r3, [r3]
-	@mov	r0, r2
-	mov	r1, r3
+	mov	r1, r4
 	bl	softPwmWrite
-	@ldr	r3, .L7
-	@ldr	r3, [r3]
-	sub	r3, r3, #1
-	@ldr	r2, .L7
-	@str	r3, [r2]
+	sub	r4, r4, #1
 	mov	r0, #40
 	bl	delay
 .L4:
-	@ldr	r3, .L7
-	@ldr	r3, [r3]
-	cmp	r3, #0
+	cmp	r4, #0
 	bgt	.L5
 	mov	r0, #7
-	@mov	r0, r3
 	mov	r1, #0
 	bl	softPwmWrite
-	mov	r3, #0
-	mov	r0, r3
-	ldmfd	sp!, {fp, pc}
+	pop {ip, pc}
 
 	
