fc7276b85be7011239990e1ea7e0b92643fc8a27
Nate Lundie <ngl8@case.edu>
2017-12-08 18:26:44 -0500

    loop

---
+++ Lab7/Part2/system_calls.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+#include<stdlib.h>
+
+
+
+
+
+
+
+
+int main(void)
+{
+
+	while(1)
+	{
+		printf("Temp: ");
+		system('cat /sys/bus/iio/devices/iio\:device0/in_temp_input');
+		printf("\n");
+		printf("Humidity:");
+		system('cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input');
+		printf("\n");
+		delay(50);
+	}
+
+
+
+}
