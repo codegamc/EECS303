b7e1d2aaaacaac79539fe8e614f5547c4d58b4f8
Nate Lundie <ngl8@case.edu>
2017-12-08 18:28:57 -0500

    escape

--- Lab7/Part2/system_calls.c
+++ Lab7/Part2/system_calls.c
@@ -16,10 +16,10 @@ int main(void)
 	while(1)
 	{
 		printf("Temp: ");
-		system('cat /sys/bus/iio/devices/iio\:device0/in_temp_input');
+		system("cat /sys/bus/iio/devices/iio\\:device0/in_temp_input");
 		printf("\n");
 		printf("Humidity:");
-		system('cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input');
+		system("cat /sys/bus/iio/devices/iio\\:device0/in_humidityrelative_input");
 		printf("\n");
 		delay(50);
 	}
